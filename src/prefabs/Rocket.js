fb52b2d077be45b07dd7be1fcc4dc375ddc9d5b9
Marcus Tierney <mstierne@ucsc.edu>
2025-01-15 16:28:55 -0800

    creating an animation

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,8 +2,7 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
       super(scene, x, y, texture, frame)
-  
-      // add object to existing scene
+      
       scene.add.existing(this)
       this.isFiring = false
       this.moveSpeed = 2
@@ -31,5 +30,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false
             this.y = game.config.height - borderUISize - borderPadding
         }
+    }
+
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = falsethis.y = game.config.height - borderUISize - borderPadding
     }
 }
